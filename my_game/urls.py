4a47cdb3f1f7f2379579270cb50ae1e37007c581
tadej <tolik20002@bigmir.net>
2015-03-02 00:37:11 +0200

    Рефакторинг. Строительство

--- my_game/urls.py
+++ my_game/urls.py
@@ -5,6 +5,7 @@ from my_game.knowledge import views as knowledge
 from my_game.civilization import views as civilization
 from my_game.warehouse import views as warehouse
 from my_game.administrator import views as administration
+from my_game.building import views as building
 
 # import my_game.registration.registration
 
@@ -28,19 +29,22 @@ urlpatterns = patterns('',
                        url(r'^civilization', civilization.civilization),
                        url(r'^warehouse', warehouse.warehouse),
 
-
                        url(r'^knowledge', knowledge.knowledge),
                        url(r'^study', knowledge.study),
 
-                       url(r'building', 'my_game.views.building', name='building'),
-                       url(r'choice_build', 'my_game.views.choice_build', name="choice_build"),
-                       url(r'working', 'my_game.views.working', name='working'),
+                       url(r'^building', building.building),
+                       url(r'^choice_build', building.choice_build),
+                       url(r'^working', building.working),
+
+
                        url(r'factory', 'my_game.views.factory', name='factory'),
                        url(r'choice_element', 'my_game.views.choice_element', name='choice_element'),
                        url(r'production', 'my_game.views.production', name='production'),
+
                        url(r'designingships', 'my_game.views.designingships', name='designingships'),
                        url(r'new_ship', 'my_game.views.new_ship', name='new_ship'),
                        url(r'work_with_project', 'my_game.views.work_with_project', name='work_with_project'),
+
                        url(r'space_forces', 'my_game.views.space_forces', name='space_forces'),
                        url(r'fleet_manage', 'my_game.fleet_management.fleet_manage', name='fleet_manage'),
                        url(r'trade', 'my_game.views.trade', name='trade'),
