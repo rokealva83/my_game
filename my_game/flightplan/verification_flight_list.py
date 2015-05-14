c479dedadf806c2a185236bd0e9e2741563ba59e
tadej <tolik20002@bigmir.net>
2015-05-14 23:18:24 +0300

    Результат сканирования. (Без теста)

--- my_game/flightplan/verification_flight_list.py
+++ my_game/flightplan/verification_flight_list.py
@@ -4,12 +4,13 @@ from datetime import timedelta
 from django.utils import timezone
 import math
 from my_game.models import Planet
-from my_game.models import System, Asteroid_field
+from my_game.models import System, Asteroid_field, Flightplan_scan
 from my_game.models import Fleet, Fuel_pattern, Fuel_tank
 from my_game.models import Flightplan, Flightplan_flight, Fleet_parametr_scan, Flightplan_production
 from my_game.models import Mail
 from my_game.flightplan.start.start_flight import start_flight
 from my_game.flightplan.veryfication.flight_verification import verification_flight
+from my_game.flightplan.veryfication.scan_veryfication import scan_veryfication
 from my_game.flightplan import fuel
 
 
@@ -25,13 +26,20 @@ def verification_flight_list(request):
             flightplan_id = flightplan.id
             if flightplan.status == 1:
                 if flightplan.class_command == 1:
-                    finish_time = verification_flight(fleet, flightplan)
+                    finish_time = verification_flight(fleet)
+                    flightplan = Flightplan.objects.filter(id_fleet=fleet.id).first()
+
+
+                elif flightplan.class_command == 6:
+                    scan_veryfication(fleet)
                     flightplan = Flightplan.objects.filter(id_fleet=fleet.id).first()
 
             if flightplan:
                 start_flight(fleet.id, finish_time)
             else:
                 fleet_up = Fleet.objects.filter(id=fleet.id).update(status=0)
+
+
 
 
 
@@ -49,88 +57,22 @@ def verification_flight_list(request):
                 # new_time_extraction =  flightplan_production.time_extraction - new_delta
                 # extraction_mine = new_delta * flightplan_production.production_per_minute
                 # resource1 = extraction_mine * asteroid_field.koef_res_1
-                #         resource2 = extraction_mine * asteroid_field.koef_res_2
-                #         resource3 = extraction_mine * asteroid_field.koef_res_3
-                #         resource4 = extraction_mine * asteroid_field.koef_res_4
-                #         mineral1 = extraction_mine * asteroid_field.koef_min_1
-                #         mineral2 = extraction_mine * asteroid_field.koef_min_2
-                #         mineral3 = extraction_mine * asteroid_field.koef_min_3
-                #         mineral4 = extraction_mine * asteroid_field.koef_min_4
+                # resource2 = extraction_mine * asteroid_field.koef_res_2
+                # resource3 = extraction_mine * asteroid_field.koef_res_3
+                # resource4 = extraction_mine * asteroid_field.koef_res_4
+                # mineral1 = extraction_mine * asteroid_field.koef_min_1
+                # mineral2 = extraction_mine * asteroid_field.koef_min_2
+                # mineral3 = extraction_mine * asteroid_field.koef_min_3
+                # mineral4 = extraction_mine * asteroid_field.koef_min_4
                 #
-                # elif flightplan.class_command == 6:
-                #     system = System.objects.filter(id=fleet.system).first()
-                #     fleet_parametr_scan = Fleet_parametr_scan.objects.filter(fleet_id=fleet.id,
-                #                                                              method_scanning=flightplan.id_command).first()
+
                 #
-                #     if fleet.planet_status == 1:
-                #         fleet_x = (int(system.x) * 1000 + int(fleet.x)) / 1000.0
-                #         fleet_y = (int(system.y) * 1000 + int(fleet.y)) / 1000.0
-                #         fleet_z = (int(system.z) * 1000 + int(fleet.z)) / 1000.0
-                #     else:
-                #         fleet_x = int(fleet.x)
-                #         fleet_y = int(fleet.y)
-                #         fleet_z = int(fleet.z)
                 #
-                #     delta = fleet_parametr_scan.range_scanning
-                #     max_x = fleet_x + delta
-                #     min_x = fleet_x - delta
-                #     max_y = fleet_y + delta
-                #     min_y = fleet_y - delta
-                #     max_z = fleet_z + delta
-                #     min_z = fleet_z - delta
-                #     systems = System.objects.filter()
-                #     asteroid_fields = Asteroid_field.objects.filter()
-                #     if flightplan.id_command == 1:
-                #         mail = 'Координаты систем:' + '\n'
-                #         for system in systems:
-                #             if min_x <= system.x <= max_x and min_y <= system.y <= max_y and min_z <= system.z <= max_z:
-                #                 system_x = system.x
-                #                 system_y = system.y
-                #                 system_z = system.z
-                #                 distance = round(math.sqrt(
-                #                     (fleet_x - system_x) ** 2 + (fleet_y - system_y) ** 2 + (fleet_z - system_z) ** 2),
-                #                     3)
-                #                 if distance <= delta and distance != 0:
-                #                     line = str(system_x) + ':' + str(system_y) + ':' + str(
-                #                         system_z) + ' Растояние:' + str(
-                #                         distance) + ' св.' + ' \n'
-                #                     mail = mail + line
-                #         final_mail = mail
-                #
-                #     elif flightplan.id_command == 2:
-                #         size = 0
-                #         ast_mail = '\n' + 'Координаты астероидных полей:' + '\n'
-                #         for asteroid_field in asteroid_fields:
-                #             if min_x <= asteroid_field.x <= max_x and min_y <= asteroid_field.y <= max_y and min_z <= asteroid_field.z <= max_z:
-                #                 system_x = asteroid_field.x
-                #                 system_y = asteroid_field.y
-                #                 system_z = asteroid_field.z
-                #                 asteroid_size = asteroid_field.size
-                #                 size = size + asteroid_field.size
-                #                 distance = round(math.sqrt(
-                #                     (fleet_x - system_x) ** 2 + (fleet_y - system_y) ** 2 + (fleet_z - system_z) ** 2),
-                #                     3)
-                #                 if distance <= delta:
-                #                     line = str(system_x) + ' : ' + str(system_y) + ' : ' + str(system_z) + ' (' + str(
-                #                         asteroid_size) + ') ' + ' Растояние:' + str(distance) + ' св.' + '\n'
-                #                     ast_mail = ast_mail + line
-                #         final_mail = ast_mail
-                #
-                #     mail = Mail(
-                #         user=user,
-                #         recipient=0,
-                #         time=datetime.now(),
-                #         status=1,
-                #         category=4,
-                #         login_recipient='Система',
-                #         title='Отчет сканирования',
-                #         message=final_mail
-                #     )
                 #
                 # lens = lens + 1
                 # if lens == flightplan_len:
-                #     fleet_up = Fleet.objects.filter(id=fleet.id).update(status=0)
+                # fleet_up = Fleet.objects.filter(id=fleet.id).update(status=0)
                 # else:
-                #     flightplan = Flightplan.objects.filter(id_fleet=fleet.id).first().update(status=1)
+                # flightplan = Flightplan.objects.filter(id_fleet=fleet.id).first().update(status=1)
                 #
                 # flightplan = Flightplan.objects.filter(id=flightplan_id).delete()
--- my_game/flightplan/veryfication/scan_veryfication.py
+++ my_game/flightplan/veryfication/scan_veryfication.py
@@ -1,1 +1,160 @@
-__author__ = 'tadej'
+# -*- coding: utf-8 -*-
+
+from django.utils import timezone
+import math
+from my_game.models import Planet
+from my_game.models import System, Asteroid_field, Flightplan_scan
+from my_game.models import Fleet, Fuel_pattern, Fuel_tank
+from my_game.models import Flightplan, Fleet_parametr_scan
+from my_game.models import Mail
+from my_game.flightplan.veryfication.flight_verification import verification_flight
+from my_game.flightplan import fuel
+
+def scan_veryfication(*args):
+    fleet = args[0]
+    user = fleet.user
+    flightplan = Flightplan.objects.filter(id_fleet=fleet.id).first()
+
+    mail_subject = ''
+    final_mail = ''
+    flightplan_scan = Flightplan_scan.objects.filter(id_fleetplan=flightplan.id).first()
+
+    time = timezone.now()
+    time_start = flightplan_scan.start_time
+    delta_time = time - time_start
+    new_delta = delta_time.seconds
+    delta = flightplan_scan.flight_time
+    if new_delta > delta:
+        fleet_parametr_scan = Fleet_parametr_scan.objects.filter(fleet_id=fleet.id,
+                                                                 method_scanning=flightplan.id_command).first()
+        fleet_x = int(fleet.x)
+        fleet_y = int(fleet.y)
+        fleet_z = int(fleet.z)
+
+        delta = fleet_parametr_scan.range_scanning
+        max_x = fleet_x + delta
+        min_x = fleet_x - delta
+        max_y = fleet_y + delta
+        min_y = fleet_y - delta
+        max_z = fleet_z + delta
+        min_z = fleet_z - delta
+
+        if flightplan_scan == 1:
+            systems = System.objects.filter()
+            mail_subject = u'Сканирование пространства'
+            mail_text = u'Флот произвел сканирование пространства, находясь на координатам %s : %s : %s. \n Координаты систем: \n' % (
+                fleet_x, fleet_y, fleet_z)
+            message_planet_subject = u''
+            message_planet = u''
+            for system in systems:
+                if min_x <= system.x <= max_x and min_y <= system.y <= max_y and min_z <= system.z <= max_z:
+                    system_x = system.x
+                    system_y = system.y
+                    system_z = system.z
+                    distance = round(math.sqrt(
+                        (fleet_x - system_x) ** 2 + (fleet_y - system_y) ** 2 + (
+                            fleet_z - system_z) ** 2), 3)
+                    if distance <= delta and distance != 0:
+                        line = u' %s : %s : %s - растояние: %s св. \n' % (
+                            system_x, system_y, system_z, distance)
+                        if distance <= delta * 0.1:
+                            message_planet_subject = u'  В системе найдены планеты с координатами: \n'
+                            planets = Planet.objects.filter(system_id=system.id).order_by('planet_num')
+                            for planet in planets:
+                                message_planet = u'    %s - %s : %s : %s \n' % (
+                                    planet.planet_num, planet.global_x, planet.global_y,
+                                    planet.global_z)
+                        mail_text = mail_text + line + message_planet_subject + message_planet
+            final_mail = mail_text
+
+
+        elif flightplan.id_command == 2:
+            asteroid_fields = Asteroid_field.objects.filter()
+            size = 0
+            mail_subject = u'Поиск астероидных полей \n'
+            ast_mail = u'Флот произвел поиск астероидов, находясь на координатам %s : %s : %s.\n Координаты астероидных полей: \n'
+            for asteroid_field in asteroid_fields:
+                if min_x <= asteroid_field.x <= max_x and min_y <= asteroid_field.y <= max_y and min_z <= asteroid_field.z <= max_z:
+                    system_x = asteroid_field.x
+                    system_y = asteroid_field.y
+                    system_z = asteroid_field.z
+                    asteroid_size = asteroid_field.size
+                    size = size + asteroid_field.size
+                    distance = round(math.sqrt(
+                        (fleet_x - system_x) ** 2 + (fleet_y - system_y) ** 2 + (
+                            fleet_z - system_z) ** 2),
+                        3)
+                    if distance <= delta:
+                        line = u'%s : %s : %s  Размер:%s Растояние:%s \n' % (
+                            system_x, system_y, system_z, asteroid_size, distance)
+                        ast_mail = ast_mail + line
+            size = str(size)
+            final_mail = ast_mail + size
+
+        elif flightplan_scan.id_command == 3:
+            fleets = Fleet.objects.filter(status=1)
+
+            for fleet in fleets:
+                verification_flight(fleet)
+            fleets = Fleet.objects.all()
+            mail_subject = u'Сканирование пространства \n'
+            fleet_mail = u'Флот произвел сканирование, находясь на координатам %s : %s : %s.\n Координаты найденых флотов: \n'
+            for fleet in fleets:
+                flightplan = Flightplan.objects.filter(id_fleet=fleet.id).first()
+                if flightplan.class_command == 1 and flightplan.id_command != 4:
+                    if min_x <= fleet.x <= max_x and min_y <= fleet.y <= max_y and min_z <= fleet.z <= max_z:
+                        system_x = fleet.x
+                        system_y = fleet.y
+                        system_z = fleet.z
+                        line = u'%s : %s : %s \n' % (system_x, system_y, system_z)
+                        fleet_mail = fleet_mail + line
+
+                elif flightplan.class_command != 1:
+                    if min_x <= fleet.x <= max_x and min_y <= fleet.y <= max_y and min_z <= fleet.z <= max_z:
+                        system_x = fleet.x
+                        system_y = fleet.y
+                        system_z = fleet.z
+                        line = u'%s : %s : %s \n' % (system_x, system_y, system_z)
+                        fleet_mail = fleet_mail + line
+                final_mail = fleet_mail
+
+        elif flightplan_scan.id_command == 4:
+            flightplans = Flightplan.objects.filter(status=1, class_command=1, id_commant=4)
+            for flightplan in flightplans:
+                fleet = Fleet.objects.filter(id=flightplan.id_fleet).first()
+                verification_flight(fleet)
+                mail_subject = u'Сканирование гиперпространства \n'
+                fleet_mail = u'Флот произвел сканирование, находясь на координатам %s : %s : %s.\n Координаты найденых в гипере флотов: \n'
+                if min_x <= fleet.x <= max_x and min_y <= fleet.y <= max_y and min_z <= fleet.z <= max_z:
+                    system_x = fleet.x
+                    system_y = fleet.y
+                    system_z = fleet.z
+                    line = u'%s : %s : %s \n' % (system_x, system_y, system_z)
+                    fleet_mail = fleet_mail + line
+                final_mail = fleet_mail
+
+        mail = Mail(
+            user=user,
+            recipient=0,
+            time=timezone.now(),
+            status=1,
+            category=4,
+            login_recipient='Система',
+            title=mail_subject,
+            message=final_mail
+        )
+        fuel_tank = Fuel_tank.objects.filter(fleet_id=fleet.id).first()
+        fuel_pattern = Fuel_pattern.objects.filter(user=fleet.user, fuel_class=fuel_tank.fuel_class).first()
+
+        need_fuel = fuel.fuel_scan(fleet.id, flightplan_scan, flightplan)
+        need_fuel = need_fuel / fuel_pattern.efficiency
+
+        new_fuel = int(fuel_tank.amount_fuel - need_fuel)
+        new_mass = int(fuel_tank.mass - need_fuel * fuel_pattern.mass)
+        new_size = int(fuel_tank.size - need_fuel * fuel_pattern.size)
+        fuel_tank = Fuel_tank.objects.filter(id=fuel_tank.id, fleet_id=fleet.id).update(amount_fuel=new_fuel,
+                                                                                                mass_fuel=new_mass,
+                                                                                                size_fuel=new_size)
+        flightplan_scan = Flightplan_scan.objects.filter(id_fleetplan=flightplan.id).delete()
+        flightplan = Flightplan.objects.filter(id=flightplan.id).delete()
+
