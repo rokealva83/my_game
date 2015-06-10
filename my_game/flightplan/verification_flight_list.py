edb53ccea7b8eafa324290ce7fc39459a28d517c
rokealva83 <rokealva83@gmail.com>
2015-06-10 19:24:50 +0300

    пересчет времиени при добыче. незакончено

--- my_game/flightplan/verification_flight_list.py
+++ my_game/flightplan/verification_flight_list.py
@@ -34,66 +34,72 @@ def verification_flight_list(request):
                 elif flightplan.class_command == 3:
                     flightplan_extraction = Flightplan_production.objects.filter(id_fleetplan=flightplan.id).first()
                     if flightplan_extraction:
-                        finish_time = timezone.now()
+
                         time = timezone.now()
                         time_start = flightplan_extraction.start_time
+                        time_extraction = int(flightplan_extraction.time_extraction)
                         delta_time = time - time_start
                         new_delta = delta_time.seconds
-                        delta = int(flightplan_extraction.time_extraction)
-                        if new_delta > delta:
-                            finish_time = time_start + timedelta(seconds=delta)
-                            fleet_resource_extraction = Fleet_parametr_resource_extraction.objects.filter(
-                                fleet_id=fleet.id).first()
-                            extract_per_second = int(fleet_resource_extraction.extraction_per_minute) / 60
-                            extraction = delta * extract_per_second
-                            x = fleet.x
-                            y = fleet.y
-                            z = fleet.z
+                        if time_extraction > new_delta:
+                            delta = new_delta
+                        else:
+                            delta = time_extraction
+                            finish = new_delta - time_extraction
+                            finish_time = time - timedelta(seconds=finish)
 
-                            asteroid_field = Asteroid_field.objects.filter(x=x, y=y, z=z).first()
-                            if asteroid_field:
-                                if extraction > fleet.empty_hold:
-                                    extraction = fleet.empty_hold
-                                    if extraction > asteroid_field.size:
-                                        extraction = asteroid_field.size
-                                        asteroid_field = Asteroid_field.objects.filter(x=x, y=y, z=z).delete()
-                                    else:
-                                        new_size = asteroid_field.size - extraction
-                                        asteroid_field = Asteroid_field.objects.filter(x=x, y=y, z=z).update(size=new_size)
+                        fleet_resource_extraction = Fleet_parametr_resource_extraction.objects.filter(
+                            fleet_id=fleet.id).first()
+                        extract_per_second = int(fleet_resource_extraction.extraction_per_minute) / 60
+                        extraction = delta * extract_per_second
+                        x = fleet.x
+                        y = fleet.y
+                        z = fleet.z
 
-                                resource1 = extraction * asteroid_field.koef_res_1
-                                resource2 = extraction * asteroid_field.koef_res_2
-                                resource3 = extraction * asteroid_field.koef_res_3
-                                resource4 = extraction * asteroid_field.koef_res_4
-                                mineral1 = extraction * asteroid_field.koef_min_1
-                                mineral2 = extraction * asteroid_field.koef_min_2
-                                mineral3 = extraction * asteroid_field.koef_min_3
-                                mineral4 = extraction * asteroid_field.koef_min_4
+                        asteroid_field = Asteroid_field.objects.filter(x=x, y=y, z=z).first()
+                        if asteroid_field:
+                            if extraction > fleet.empty_hold:
+                                extraction = fleet.empty_hold
+                                if extraction > asteroid_field.size:
+                                    extraction = asteroid_field.size
+                                    asteroid_field = Asteroid_field.objects.filter(x=x, y=y, z=z).delete()
+                                else:
+                                    new_size = asteroid_field.size - extraction
+                                    asteroid_field = Asteroid_field.objects.filter(x=x, y=y, z=z).update(size=new_size)
 
-                                res1 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=1).first()
-                                res2 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=2).first()
-                                res3 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=3).first()
-                                res4 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=4).first()
-                                min1 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=5).first()
-                                min2 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=6).first()
-                                min3 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=7).first()
-                                min4 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=8).first()
+                            resource1 = extraction * asteroid_field.koef_res_1
+                            resource2 = extraction * asteroid_field.koef_res_2
+                            resource3 = extraction * asteroid_field.koef_res_3
+                            resource4 = extraction * asteroid_field.koef_res_4
+                            mineral1 = extraction * asteroid_field.koef_min_1
+                            mineral2 = extraction * asteroid_field.koef_min_2
+                            mineral3 = extraction * asteroid_field.koef_min_3
+                            mineral4 = extraction * asteroid_field.koef_min_4
 
-                                add_res(fleet, res1, resource1)
-                                add_res(fleet, res2, resource2)
-                                add_res(fleet, res3, resource3)
-                                add_res(fleet, res4, resource4)
-                                add_res(fleet, min1, mineral1)
-                                add_res(fleet, min2, mineral2)
-                                add_res(fleet, min3, mineral3)
-                                add_res(fleet, min4, mineral4)
+                            res1 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=1).first()
+                            res2 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=2).first()
+                            res3 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=3).first()
+                            res4 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=4).first()
+                            min1 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=5).first()
+                            min2 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=6).first()
+                            min3 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=7).first()
+                            min4 = Hold.objects.filter(fleet_id=fleet.id, class_shipment=0, id_shipment=8).first()
 
-                                new_hold = fleet.hold + extraction
-                                mass = extraction
-                                new_mass = fleet.ship_empty_mass + mass
-                                new_empty_hold = fleet.empty_hold - extraction
+                            add_res(fleet, res1, resource1)
+                            add_res(fleet, res2, resource2)
+                            add_res(fleet, res3, resource3)
+                            add_res(fleet, res4, resource4)
+                            add_res(fleet, min1, mineral1)
+                            add_res(fleet, min2, mineral2)
+                            add_res(fleet, min3, mineral3)
+                            add_res(fleet, min4, mineral4)
 
-                                fleet_up = Fleet.objects.filter(id=fleet.id).update(hold=new_hold, ship_empty_mass=new_mass, empty_hold=new_empty_hold)
+                            new_hold = fleet.hold + extraction
+                            mass = extraction
+                            new_mass = fleet.ship_empty_mass + mass
+                            new_empty_hold = fleet.empty_hold - extraction
+                            fleet_up = Fleet.objects.filter(id=fleet.id).update(hold=new_hold, ship_empty_mass=new_mass, empty_hold=new_empty_hold)
+                            delta=flightplan_extraction.time_extraction - new_delta
+                            flightplan_extraction = Flightplan_production.objects.filter(id_fleetplan=flightplan.id).update(start_time=time)
 
 
                         #  Дописать:
